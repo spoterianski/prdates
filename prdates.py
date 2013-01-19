bfc3d9b1e06623ba194b8f4626111cde0931d0c6
Sergey Poterianski <spoterianski@gmail.com>
2013-01-19 17:04:02 +0400

    Refactoring - rename class name and variables. Make the method for print month ranges. Write description and usage text

--- prdates.py
+++ prdates.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Sergey Poterianski
-#
+# coding: utf-8
+# 2013 by Sergey Poterianski
 #
 import argparse
 import re
 from datetime import date
 from datetime import timedelta
+from dateutil.relativedelta import relativedelta
 
 
-class PrintDays:
+class PrintDates:
 	"""
-	PrintDays - this is small tool for printing string yyyymmdd between two dates
+	PrintDates - this is small tool for printing string yyyymmdd between two dates
 	"""
 	def __init__(self):
-		self.rxDate = re.compile('(?P<year>\d{4})-{,1}(?P<month>\d{2})-{,1}(?P<day>\d{2})')
-		self.delta_d = timedelta(days = 1)
-		self.delta_w = timedelta(days = 7)
-		self.delta_w2 = timedelta(days = 6)
-
+		self.rx_date = re.compile('(?P<year>\d{4})-{,1}(?P<month>\d{2})-{,1}(?P<day>\d{2})')
+		self.delta_day = timedelta(days = 1)
+		self.delta_week = timedelta(days = 7)
+		self.delta_fdays6 = timedelta(days = 6)
+		
 
 	def Print(self, mode, begin, end):
-		match_begin = self.rxDate.match(begin)
-		match_end = self.rxDate.match(end)
+		match_begin = self.rx_date.match(begin)
+		match_end = self.rx_date.match(end)
 		if match_begin is None or match_end is None:
 			return 1
 
@@ -36,26 +36,40 @@ class PrintDays:
 
 		
 		if mode == 'd':
+			# print day
 			day = bday
 			while day <= eday:
 				print day.strftime('%Y%m%d')
-				day += self.delta_d
-		
+				day += self.delta_day
 		elif mode == 'w':
-			day = bday - (self.delta_d * bday.weekday())
+			# print week range
+			day = bday - (self.delta_day * bday.weekday())
 			while day <= eday:
-				print day.strftime('%Y%m%d') + ' ' + (day + self.delta_w2).strftime('%Y%m%d')
-				day += self.delta_w
+				print day.strftime('%Y%m%d') + ' ' + (day + self.delta_fdays6).strftime('%Y%m%d')
+				day += self.delta_week
+		elif mode == 'm':
+			# print month range
+			day = bday.replace(day=1)
+			while day <= eday:
+				print day.strftime('%Y%m%d') + ' ' + (day + relativedelta(months=1) - \
+					relativedelta(days=1)).strftime('%Y%m%d')
+				day += relativedelta(months=1)
 		return 0
 
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='PrintDays - this is small tool for printing string yyyymmdd between two dates')
-	parser.add_argument('-m', '--mode', choices='dwm', help='Print mode: d - day, w - week, m - month (default=d)', default='d')
+	# parce command arguments
+	parser = argparse.ArgumentParser(description='PrintDays - this is small'\
+		' tool for printing string yyyymmdd between two dates')
+	parser.add_argument('-m', '--mode', choices='dwm', help='Print mode: d - day, '\
+		'w - week, m - month (default=d)', default='d')
 	parser.add_argument('-b', '--begin', help='Begin date', required=True)
 	parser.add_argument('-e', '--end', help='End date', required=True)
 	args = parser.parse_args()
 
-	pdays = PrintDays()
-	if pdays.Print(args.mode, args.begin, args.end) != 0:
+	# print date ranges
+	pdates = PrintDates()
+	if pdates.Print(args.mode, args.begin, args.end) != 0:
 		parser.print_usage()
+	
+
--- readme.md
+++ readme.md
@@ -1,5 +1,20 @@
-# prdates.py
-this is small tool for printing string "yyyymmdd" between two dates
+-- prdates.py
+This is small tool for printing string "yyyymmdd" between two dates
 
-Sergey Poterianski
-2012.05.29
+2013 by Sergey Poterianski
+
+In my work I'm often use dates as string "yyyymmdd" and I decided to write a small utility for convenient print a date range in between two dates.
+
+Usage:
+prdates.py [-b|--begin] [begin date] [-e|--end] [end date] [-m|--mode] [d|w|m]
+
+-b or --begin - Begin date in YYYYMMDD format
+-e or --end - End date in  YYYYMMDD format
+-m or --mode - Range: d - day, w - week, m - month
+
+Examples: 
+./prdates.py -b 20120101 -e 20130201 -m m - print month ranges between 2012.01.01 and 2013.02.01
+./prdates.py -b 20120101 -e 20130201 -m w - print week ranges between 2012.01.01 and 2013.02.01
+./prdates.py -b 20120101 -e 20130201 -m d - print dates between 2012.01.01 and 2013.02.01
+
+
